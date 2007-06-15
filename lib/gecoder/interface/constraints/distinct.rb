3523c294f11ebe2202004f7507ab54dca89c6162
Andreas Launila <github@lokorin.org>
2007-06-15 11:47:43 +0000

    Added a comment for the distinct method.

--- lib/gecoder/interface/constraints/distinct.rb
+++ lib/gecoder/interface/constraints/distinct.rb
@@ -1,5 +1,6 @@
 module Gecode
   class IntVarEnumConstraintExpression
+    # Posts a distinct constraint on the variables in the enum.
     def distinct
       if @negate
         # The best we could implement it as from here would be a bunch of 
