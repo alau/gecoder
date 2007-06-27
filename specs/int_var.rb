a58b8aef11109006c1ead4f7dcca3d5e58721c2a
Andreas Launila <github@lokorin.org>
2007-06-27 13:04:53 +0000

    A fix submitted by David for bug [#11861], slightly modified by Andreas.

--- specs/int_var.rb
+++ specs/int_var.rb
@@ -31,6 +31,10 @@ describe 'non-empty int variable', :shared => true do
   it 'should give a NoMethodError when calling a method that doesn\'t exist' do
     lambda{ @var.this_method_does_not_exists }.should raise_error(NoMethodError)
   end
+  
+  it 'should have a zero degree' do
+    @var.degree.should be_zero
+  end
 end
 
 describe Gecode::FreeIntVar, ' (with range domain of size > 1)' do
@@ -42,10 +46,6 @@ describe Gecode::FreeIntVar, ' (with range domain of size > 1)' do
   end
   
   it_should_behave_like 'non-empty int variable'
-  
-  it 'should have a zero degree' do
-    @var.degree.should be_zero
-  end
   
   it 'should not be assigned' do
     @var.should_not be_assigned
@@ -67,22 +67,6 @@ describe Gecode::FreeIntVar, ' (defined with three-dot range)' do
   it_should_behave_like 'non-empty int variable'
 end
 
-# Many of these fail. It does not appear to be a problem in the Ruby-code, 
-# rather IntSet (in Model#int_var) seems to behave strange when e.g. given an 
-# array of [1,3,5]. Example:
-# 
-# > set = Gecode::Raw::IntSet.new([1,3,5], 3)
-# > set.size
-# 3    <- This is correct
-# > set.min(0)
-# 4    <- This is not correct, 1 is expected.
-# > set.min(1)
-# -1037935314    <- This is not correct, 3 is expected.
-# 
-# It works fine through Gecode/J, so it's probably not something in Gecode
-# that's causing the strange behavior. Possibly it's something in the 
-# conversion of the arrays, but I can't find any other method that uses
-# constant arrays to test with.
 describe Gecode::FreeIntVar, ' (with non-range domain of size > 1)' do
   before do
     @domain = [-3, -2, -1, 1]
@@ -91,10 +75,6 @@ describe Gecode::FreeIntVar, ' (with non-range domain of size > 1)' do
   end
 
   it_should_behave_like 'non-empty int variable'
-  
-  it 'should have a degree equal to the domain degree' do
-    @var.degree.should equal(1)
-  end
   
   it 'should not be assigned' do
     @var.should_not be_assigned
@@ -105,7 +85,7 @@ describe Gecode::FreeIntVar, ' (with non-range domain of size > 1)' do
   end
   
   it 'should not contain the domain\'s holes' do
-    @var.should_not be_in(1)
+    @var.should_not be_in(0)
   end
 end
 
@@ -117,10 +97,6 @@ describe Gecode::FreeIntVar, '(with a domain of size 1)' do
   end
   
   it_should_behave_like 'non-empty int variable'
-  
-  it 'should have a zero degree' do
-    @var.degree.should be_zero
-  end
   
   it 'should be assigned' do
     @var.should be_assigned
