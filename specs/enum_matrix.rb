88cd9cd8bb037b884342642de2bad40521df1452
Andreas Launila <github@lokorin.org>
2007-06-29 08:13:38 +0000

    Plugged a hole in the C0 coverage.

---
+++ specs/enum_matrix.rb
@@ -0,0 +1,24 @@
+require File.dirname(__FILE__) + '/spec_helper'
+
+describe Gecode::Util::EnumMatrix do
+  before do
+    @matrix = Gecode::Util::EnumMatrix[[0, 1], [1, 0]]
+  end
+  
+  it 'should be enumerable' do
+    @matrix.should be_kind_of(Enumerable)
+  end
+  
+  it 'should produce rows that are enumerable' do
+    @matrix.row(0).should be_kind_of(Enumerable)
+    @matrix.row(0).inject([]){ |arr, e| arr << e }.should == [0, 1]
+  end
+  
+  it 'should produce columns that are enumerable' do
+    @matrix.column(0).should be_kind_of(Enumerable)
+  end
+  
+  it 'should produce submatrices that are enumerable' do
+    @matrix.minor(0,1,0,1).should be_kind_of(Enumerable)
+  end
+end
