63db1325110839c2cd3f47d591ad79142d4e4a89
Andreas Launila <github@lokorin.org>
2007-07-06 12:58:18 +0000

    Added disjunction for boolean enumerations.

--- lib/gecoder/interface/constraints/bool_enum/boolean.rb
+++ lib/gecoder/interface/constraints/bool_enum/boolean.rb
@@ -6,6 +6,13 @@ module Gecode
       return Gecode::Constraints::BoolEnum::ConjunctionStub.new(
         @model, :lhs => self)
     end
+    
+    # Produces an expression that can be handled as if it was a variable 
+    # representing the disjunction of all boolean variables in the enumeration.
+    def disjunction
+      return Gecode::Constraints::BoolEnum::DisjunctionStub.new(
+        @model, :lhs => self)
+    end
   end
   
   module Constraints::BoolEnum
@@ -24,6 +31,26 @@ module Gecode
           bound = variable
         end
         Gecode::Raw::bool_and(@model.active_space, enum.to_bool_var_array, 
+          bound, strength)
+        return variable
+      end
+    end
+    
+    # Describes an expression stub started with a bool var enum following by 
+    # #disjunction.
+    class DisjunctionStub < Gecode::Constraints::Bool::CompositeStub
+      def constrain_equal(variable, params)
+        enum, strength = @params.values_at(:lhs, :strength)
+        if variable.nil?
+          variable = @model.bool_var
+        end
+        
+        if variable.respond_to? :bind
+          bound = variable.bind
+        else
+          bound = variable
+        end
+        Gecode::Raw::bool_or(@model.active_space, enum.to_bool_var_array, 
           bound, strength)
         return variable
       end
--- specs/constraints/bool_enum.rb
+++ specs/constraints/bool_enum.rb
@@ -15,6 +15,58 @@ class BoolEnumSampleProblem < Gecode::Model
   end
 end
 
+# Expects @stub, which contains the started constraint and @compute_result which 
+# computes whether the left hand side is true or not.
+describe 'bool enum constraint', :shared => true do
+  it 'should handle being constrained to be true' do
+    @stub.must_be.true
+    @model.solve!
+    @compute_result.call.should be_true
+  end
+  
+  it 'should handle being constrained to be negated true' do
+    @stub.must_not_be.true
+    @model.solve!
+    @compute_result.call.should_not be_true
+  end
+  
+  it 'should handle being constrained to be false' do
+    @stub.must_be.false
+    @model.solve!
+    @compute_result.call.should_not be_true
+  end
+  
+  it 'should handle being constrained to be negated false' do
+    @stub.must_not_be.false
+    @model.solve!
+    @compute_result.call.should be_true
+  end
+  
+  it 'should handle being constrained to be equal to a variable' do
+    @stub.must_be == @b1
+    @model.solve!
+    @compute_result.call.should == @b1.true?
+  end
+  
+  it 'should handle being constrained to not be equal to a variable' do
+    @stub.must_not_be == @b1
+    @model.solve!
+    @compute_result.call.should_not == @b1.true?
+  end
+  
+  it 'should handle being constrained to be equal to be a nested expression' do
+    @stub.must_be == (@b1 | @b2) & @b1
+    @model.solve!
+    @compute_result.call.should == (@b1.true? | @b2.true?) & @b1.true?
+  end
+  
+  it 'should handle being constrained to not be equal to be a nested expression' do
+    @stub.must_not_be == (@b1 | @b2) & @b1
+    @model.solve!
+    @compute_result.call.should_not == (@b1.true? | @b2.true?) & @b1.true?
+  end
+end
+
 describe Gecode::Constraints::BoolEnum, ' (conjunction)' do
   before do
     @model = BoolEnumSampleProblem.new
@@ -36,55 +88,43 @@ describe Gecode::Constraints::BoolEnum, ' (conjunction)' do
           an_instance_of(Gecode::Raw::BoolVar), reif_var.bind, true, strength)
       end
     end
+    
+    # For bool enum spec.
+    @stub = @bools.conjunction
+    @compute_result = lambda{ @bools.all?{ |b| b.true? } }
   end
   
-  it 'should handle being constrained to be true' do
-    @bools.conjunction.must_be.true
-    @model.solve!
-    @bools.all?{ |b| b.true? }.should be_true
-  end
-  
-  it 'should handle being constrained to be negated true' do
-    @bools.conjunction.must_not_be.true
-    @model.solve!
-    @bools.any?{ |b| b.false? }.should be_true
-  end
-  
-  it 'should handle being constrained to be false' do
-    @bools.conjunction.must_be.false
-    @model.solve!
-    @bools.any?{ |b| b.false? }.should be_true
-  end
-  
-  it 'should handle being constrained to be negated false' do
-    @bools.conjunction.must_not_be.false
-    @model.solve!
-    @bools.all?{ |b| b.true? }.should be_true
-  end
-  
-  it 'should handle being constrained to be equal to a variable' do
-    @bools.conjunction.must_be == @b1
-    @model.solve!
-    @bools.all?{ |b| b.true? }.should == @b1.true?
-  end
-  
-  it 'should handle being constrained to not be equal to a variable' do
-    @bools.conjunction.must_not_be == @b1
-    @model.solve!
-    @bools.all?{ |b| b.true? }.should_not == @b1.true?
-  end
-  
-  it 'should handle being constrained to be equal to be a nested expression' do
-    @bools.conjunction.must_be == (@b1 | @b2) & @b1
-    @model.solve!
-    @bools.all?{ |b| b.true? }.should == (@b1.true? | @b2.true?) & @b1.true?
-  end
-  
-  it 'should handle being constrained to not be equal to be a nested expression' do
-    @bools.conjunction.must_not_be == (@b1 | @b2) & @b1
-    @model.solve!
-    @bools.all?{ |b| b.true? }.should_not == (@b1.true? | @b2.true?) & @b1.true?
-  end
-
+  it_should_behave_like 'bool enum constraint'
   it_should_behave_like 'constraint with options'
-end  
+end
+
+describe Gecode::Constraints::BoolEnum, ' (disjunction)' do
+  before do
+    @model = BoolEnumSampleProblem.new
+    @bools = @model.bools
+    @b1 = @model.b1
+    @b2 = @model.b2
+    
+    # For constraint option spec.
+    @invoke_options = lambda do |hash| 
+      @bools.disjunction.must_be.equal_to(true, hash) 
+      @model.solve!
+    end
+    @expect_options = lambda do |strength, reif_var|
+      Gecode::Raw.should_receive(:bool_or).once.with(@model.active_space, 
+        an_instance_of(Gecode::Raw::BoolVarArray), 
+        an_instance_of(Gecode::Raw::BoolVar), strength)
+      unless reif_var.nil?
+        Gecode::Raw.should_receive(:bool_eqv).once.with(@model.active_space, 
+          an_instance_of(Gecode::Raw::BoolVar), reif_var.bind, true, strength)
+      end
+    end
+    
+    # For bool enum spec.
+    @stub = @bools.disjunction
+    @compute_result = lambda{ @bools.any?{ |b| b.true? } }
+  end
+  
+  it_should_behave_like 'bool enum constraint'
+  it_should_behave_like 'constraint with options'
+end
