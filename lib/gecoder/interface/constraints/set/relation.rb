6fb5c16185c10b2429b8aa61f8f46e540fcd00bc
Andreas Launila <github@lokorin.org>
2007-07-10 20:02:44 +0000

    Added the set elements relation constraint.

--- lib/gecoder/interface/constraints/set/relation.rb
+++ lib/gecoder/interface/constraints/set/relation.rb
@@ -1,3 +1,15 @@
+module Gecode
+  class FreeSetVar
+    # Starts a constraint on all the elements of the set.
+    def elements
+      params = {:lhs => self}
+      Gecode::Constraints::SimpleExpressionStub.new(@model, params) do |m, ps|
+        Gecode::Constraints::Set::Relation::ElementExpression.new(m, ps)
+      end
+    end
+  end
+end
+
 module Gecode::Constraints::Set
   class Expression
     Gecode::Constraints::Util::SET_RELATION_TYPES.each_pair do |name, type|
@@ -73,6 +85,43 @@ module Gecode::Constraints::Set
         Gecode::Raw::rel(@model.active_space, *params)
       end
       negate_using_reification
+    end
+    
+    # Describes a relation constraint on the elements of a set.
+    class ElementRelationConstraint < Gecode::Constraints::Constraint
+      def post
+        var, rhs, relation = @params.values_at(:lhs, :rhs, :relation)
+        
+        if @params[:negate]
+          type = Gecode::Constraints::Util::NEGATED_RELATION_TYPES[relation]
+        else
+          type = Gecode::Constraints::Util::RELATION_TYPES[relation]
+        end
+
+        if rhs.kind_of? Fixnum
+          # Use a proxy int variable to cover.
+          rhs = @model.int_var(rhs)
+        end
+        Gecode::Raw::rel(@model.active_space, var.bind, type, rhs.bind)
+      end
+    end
+    
+    # Describes an expression which starts with set.element.must* .
+    class ElementExpression < Gecode::Constraints::Expression
+      Gecode::Constraints::Util::RELATION_TYPES.each_key do |name|
+        module_eval <<-"end_code"
+          # Creates an elements constraint using the specified expression, which
+          # may be either a constant integer of variable.
+          def #{name}(expression, options = {})
+            unless expression.kind_of?(Fixnum) or 
+                expression.kind_of?(Gecode::FreeIntVar)
+              raise TypeError, "Invalid expression type \#{expression.class}."
+            end
+            @params.update(:rhs => expression, :relation => :#{name})
+            @model.add_constraint ElementRelationConstraint.new(@model, @params)
+          end
+        end_code
+      end
     end
   end
 end
--- specs/constraints/set_relation.rb
+++ specs/constraints/set_relation.rb
@@ -109,3 +109,68 @@ describe Gecode::Constraints::Set::Relation, ' (equality)' do
   
   it_should_behave_like 'constraint with options'
 end
+
+describe Gecode::Constraints::Set::Relation, ' (elements)' do
+  include GecodeR::Specs::SetHelper
+
+  before do
+    @model = Gecode::Model.new
+    @set = @model.set_var([0], 0..2)
+    @int_var = @model.int_var(0..2)
+    @int_constant = 2
+    @model.branch_on @model.wrap_enum([@set])
+    @expect = lambda do |relation_type, rhs|
+      if rhs.kind_of? Fixnum
+        rhs = an_instance_of(Gecode::Raw::IntVar)
+      end
+      rhs = rhs.bind if rhs.respond_to? :bind
+      Gecode::Raw.should_receive(:rel).once.with(@model.active_space, 
+        @set.bind, relation_type, rhs)
+    end
+  end
+  
+  Gecode::Constraints::Util::RELATION_TYPES.each_pair do |relation, type|
+    it "should translate #{relation} with variable to relation constraint" do
+      @expect.call(type, @int_var)
+      @set.elements.must.send(relation, @int_var)
+      @model.solve!
+    end
+    
+    it "should translate #{relation} with constant to relation constraint" do
+      @expect.call(type, @int_constant)
+      @set.elements.must.send(relation, @int_constant)
+      @model.solve!
+    end
+  end
+
+  Gecode::Constraints::Util::NEGATED_RELATION_TYPES.each_pair do |relation, type|
+    it "should translate negated #{relation} with variable to relation constraint" do
+      @expect.call(type, @int_var)
+      @set.elements.must_not.send(relation, @int_var)
+      @model.solve!
+    end
+    
+    it "should translate negated #{relation} with constant to relation constraint" do
+      @expect.call(type, @int_constant)
+      @set.elements.must_not.send(relation, @int_constant)
+      @model.solve!
+    end
+  end
+  
+  it 'should constrain the elements of the set' do
+    @set.elements.must <= @int_var
+    @int_var.must == 0
+    @model.solve!
+    @set.should be_assigned
+    @set.should include(0)
+    @set.should_not include(1,2)
+  end
+  
+  it 'should constrain the elements of the set (constant parameter)' do
+    @set.elements.must <= 0
+    @model.solve!
+    @set.should be_assigned
+    @set.should include(0)
+    @set.should_not include(1,2)
+  end
+end
