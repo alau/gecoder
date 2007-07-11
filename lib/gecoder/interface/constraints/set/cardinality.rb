78fac1fde2c2579169e4c53c7dd5cf46520ff136
Andreas Launila <github@lokorin.org>
2007-07-11 17:10:37 +0000

    Added set cardinality constraints.

--- lib/gecoder/interface/constraints/set/cardinality.rb
+++ lib/gecoder/interface/constraints/set/cardinality.rb
@@ -3,9 +3,7 @@ module Gecode
     # Starts a constraint on all the size of the set.
     def size
       params = {:lhs => self}
-      Gecode::Constraints::SimpleExpressionStub.new(@model, params) do |m, ps|
-        Gecode::Constraints::Set::Cardinality::Expression.new(m, ps)
-      end
+      Gecode::Constraints::Set::Cardinality::SizeExpressionStub.new(@model, params)
     end
   end
 end
@@ -14,8 +12,24 @@ module Gecode::Constraints::Set
   # A module that gathers the classes and modules used in cardinality 
   # constraints.
   module Cardinality
+    # Describes a cardinality constraint.
+    class CardinalityConstraint < Gecode::Constraints::Constraint
+      def post
+        var, range = @params.values_at(:lhs, :range)
+        Gecode::Raw::cardinality(@model.active_space, var.bind, range.first, 
+          range.last)
+      end
+    end
+    
+    # A custom composite stub to change the composite expression used.
+    class CompositeStub < Gecode::Constraints::CompositeStub
+      def initialize(model, params)
+        super(Expression, model, params)
+      end
+    end
+    
     # Describes a cardinality expression started with set.size.must .
-    class Expression < Gecode::Constraints::Expression
+    class Expression < Gecode::Constraints::Int::CompositeExpression
       def in(range)
         unless range.kind_of? Range
           raise TypeError, "Expected Range, got #{range.class}."
@@ -29,13 +43,17 @@ module Gecode::Constraints::Set
         @model.add_constraint CardinalityConstraint.new(@model, @params)
       end
     end
-
-    # Describes a cardinality constraint.
-    class CardinalityConstraint < Gecode::Constraints::Constraint
-      def post
-        var, range = @params.values_at(:lhs, :range)
-        Gecode::Raw::cardinality(@model.active_space, var.bind, range.first, 
-          range.last)
+    
+    # Describes an expression stub started with a set variable followed by 
+    # #size .
+    class SizeExpressionStub < CompositeStub
+      def constrain_equal(variable, params)
+        lhs = @params[:lhs]
+        if variable.nil?
+          variable = @model.int_var(lhs.glb_size, lhs.lub_size)
+        end
+        Gecode::Raw::cardinality(@model.active_space, lhs.bind, variable.bind)
+        return variable
       end
     end
   end
--- specs/constraints/cardinality.rb
+++ specs/constraints/cardinality.rb
@@ -50,3 +50,76 @@ describe Gecode::Constraints::Set::Cardinality, ' (range)' do
   
   it_should_behave_like 'non-reifiable set constraint'
 end
+
+describe Gecode::Constraints::Set::Cardinality, ' (composite)' do
+  before do
+    @model = Gecode::Model.new
+    @set = @model.set_var([], 0..10)
+    @target = @var = @model.int_var(0..11)
+    @model.branch_on @model.wrap_enum([@set])
+    @model.branch_on @model.wrap_enum([@var])
+    
+    @invoke_options = lambda do |hash| 
+      @set.size.must_be.equal_to(@var, hash) 
+    end
+    
+    @invoke = lambda do |rhs| 
+      @set.size.must == rhs 
+      @model.solve!
+    end
+    @expect = lambda do |relation, rhs, strength, reif_var, negated|
+      rhs = rhs.bind if rhs.respond_to? :bind
+      if reif_var.nil?
+        if !negated and relation == Gecode::Raw::IRT_EQ and 
+            rhs.kind_of? Gecode::Raw::IntVar 
+          Gecode::Raw.should_receive(:cardinality).once.with(
+            @model.active_space, @set.bind, rhs)
+          Gecode::Raw.should_receive(:rel).exactly(0).times
+        else
+          Gecode::Raw.should_receive(:cardinality).once.with(
+            @model.active_space, @set.bind, an_instance_of(Gecode::Raw::IntVar))
+          Gecode::Raw.should_receive(:rel).once.with(@model.active_space, 
+            an_instance_of(Gecode::Raw::IntVar), relation, rhs, 
+            strength)
+        end
+      else
+        Gecode::Raw.should_receive(:cardinality).once.with(@model.active_space, 
+          @set.bind, an_instance_of(Gecode::Raw::IntVar))
+        Gecode::Raw.should_receive(:rel).once.with(@model.active_space, 
+          an_instance_of(Gecode::Raw::IntVar), relation, rhs, reif_var.bind,
+          strength)
+      end
+    end
+    
+    # For composite spec.
+    @invoke_relation = lambda do |relation, target, negated|
+      if negated
+        @set.size.must_not.send(relation, target)
+      else
+        @set.size.must.send(relation, target)
+      end
+      @model.solve!
+    end
+    @expect_relation = lambda do |relation, target, negated|
+      @expect.call(relation, target, Gecode::Raw::ICL_DEF, nil, negated)
+    end
+    
+    # For options spec.
+    @invoke_options = lambda do |hash|
+      @set.size.must_be.less_than_or_equal_to(17, hash)
+      @model.solve!
+    end
+    @expect_options = lambda do |strength, reif_var|
+      @expect.call(Gecode::Raw::IRT_LQ, 17, strength, reif_var, false)
+    end
+  end
+  
+  it 'should constrain the cardinality of a set' do
+    @set.size.must == @var
+    @model.solve!
+    @set.val_size.should == @var.val
+  end
+  
+  it_should_behave_like 'constraint with options'
+  it_should_behave_like 'composite constraint'
+end
