4cbd39d192af01d17f114eb53938c889511c5ab2
Andreas Launila <github@lokorin.org>
2007-07-12 12:49:55 +0000

    Added the set min constraint.

---
+++ lib/gecoder/interface/constraints/set/connection.rb
@@ -0,0 +1,28 @@
+module Gecode
+  class FreeSetVar
+    # Starts a constraint on the minimum value of the set.
+    def min
+      params = {:lhs => self}
+      Gecode::Constraints::Set::Connection::MinExpressionStub.new(@model, params)
+    end
+  end
+end
+
+module Gecode::Constraints::Set
+  # A module that gathers the classes and modules used in connection 
+  # constraints.
+  module Connection
+    # Describes an expression stub started with an int var following by #min.
+    class MinExpressionStub < Gecode::Constraints::Int::CompositeStub
+      def constrain_equal(variable, params)
+        set = params[:lhs]
+        if variable.nil?
+          variable = @model.int_var(set.lub_min, set.glb_min)
+        end
+        
+        Gecode::Raw::min(@model.active_space, set.bind, variable.bind)
+        return variable
+      end
+    end
+  end
+end
---
+++ specs/constraints/connection.rb
@@ -0,0 +1,72 @@
+require File.dirname(__FILE__) + '/../spec_helper'
+require File.dirname(__FILE__) + '/constraint_helper'
+
+describe Gecode::Constraints::Set::Connection, ' (min)' do
+  before do
+    @model = Gecode::Model.new
+    @set = @model.set_var([], 0..9)
+    @target = @var = @model.int_var(0..10)
+    @model.branch_on @model.wrap_enum([@set])
+    @model.branch_on @model.wrap_enum([@var])
+    @stub = @set.min
+    
+    @invoke = lambda do |rhs| 
+      @stub.must == rhs 
+      @model.solve!
+    end
+    @expect = lambda do |relation, rhs, strength, reif_var, negated|
+      rhs = rhs.bind if rhs.respond_to? :bind
+      if reif_var.nil?
+        if !negated and relation == Gecode::Raw::IRT_EQ and 
+            rhs.kind_of? Gecode::Raw::IntVar 
+          Gecode::Raw.should_receive(:min).once.with(
+            @model.active_space, @set.bind, rhs)
+          Gecode::Raw.should_receive(:rel).exactly(0).times
+        else
+          Gecode::Raw.should_receive(:min).once.with(
+            @model.active_space, @set.bind, an_instance_of(Gecode::Raw::IntVar))
+          Gecode::Raw.should_receive(:rel).once.with(@model.active_space, 
+            an_instance_of(Gecode::Raw::IntVar), relation, rhs, 
+            strength)
+        end
+      else
+        Gecode::Raw.should_receive(:min).once.with(@model.active_space, 
+          @set.bind, an_instance_of(Gecode::Raw::IntVar))
+        Gecode::Raw.should_receive(:rel).once.with(@model.active_space, 
+          an_instance_of(Gecode::Raw::IntVar), relation, rhs, reif_var.bind,
+          strength)
+      end
+    end
+    
+    # For composite spec.
+    @invoke_relation = lambda do |relation, target, negated|
+      if negated
+        @stub.must_not.send(relation, target)
+      else
+        @stub.must.send(relation, target)
+      end
+      @model.solve!
+    end
+    @expect_relation = lambda do |relation, target, negated|
+      @expect.call(relation, target, Gecode::Raw::ICL_DEF, nil, negated)
+    end
+    
+    # For options spec.
+    @invoke_options = lambda do |hash|
+      @stub.must_be.less_than_or_equal_to(17, hash)
+      @model.solve!
+    end
+    @expect_options = lambda do |strength, reif_var|
+      @expect.call(Gecode::Raw::IRT_LQ, 17, strength, reif_var, false)
+    end
+  end
+  
+  it 'should constrain the min of a set' do
+    @set.min.must == @var
+    @model.solve!
+    @set.glb_min.should == @var.val
+  end
+  
+  it_should_behave_like 'constraint with options'
+  it_should_behave_like 'composite constraint'
+end
