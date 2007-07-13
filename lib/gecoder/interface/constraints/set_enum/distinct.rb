d8074d71aea3caec63ec64ff0043fc1e24ad0fea
Andreas Launila <github@lokorin.org>
2007-07-13 18:05:55 +0000

    Added the "at most one" distinctness constraint for sets.

--- lib/gecoder/interface/constraints/set_enum/distinct.rb
+++ lib/gecoder/interface/constraints/set_enum/distinct.rb
@@ -1,7 +1,7 @@
 module Gecode::Constraints::SetEnum
   class Expression
-    # Adds a distinct constraint on the variables in the enum. The "option"
-    # :size must be specified, the sets will be constrained to that size.
+    # Adds a distinct constraint on the sets in the enum. The "option" :size 
+    # must be specified, the sets will be constrained to that size.
     def distinct(options = {})
       unless options.has_key? :size
         raise ArgumentError, 'Option :size has to be specified.'
@@ -18,6 +18,26 @@ module Gecode::Constraints::SetEnum
       @model.add_constraint Distinct::DistinctConstraint.new(
         @model, @params.update(options))
     end
+    
+    # Adds a constraint on the sets that specifies that they must have at most
+    # one element in common. The "option" :size must be specified, the sets 
+    # will be constrained to that size.
+    def at_most_share_one_element(options = {})
+      unless options.has_key? :size
+        raise ArgumentError, 'Option :size has to be specified.'
+      end
+      unless options.size == 1
+        raise ArgumentError, 'Only the option :size is accepted, got ' + 
+          "#{options.keys.join(', ')}."
+      end
+      if @params[:negate]
+        raise Gecode::MissingConstraintError, 'A negated atmost one ' + 
+          'constrain is not implemented.'
+      end
+      
+      @model.add_constraint Distinct::AtMostOneConstraint.new(
+        @model, @params.update(options))
+    end
   end
   
   # A module that gathers the classes and modules used in distinct constraints.
@@ -27,6 +47,14 @@ module Gecode::Constraints::SetEnum
       def post
         sets, size = @params.values_at(:lhs, :size)
         Gecode::Raw::distinct(@model.active_space, sets.to_set_var_array, size)
+      end
+    end
+    
+    # Describes an at most one set constraint.
+    class AtMostOneConstraint < Gecode::Constraints::Constraint
+      def post
+        sets, size = @params.values_at(:lhs, :size)
+        Gecode::Raw::atmostOne(@model.active_space, sets.to_set_var_array, size)
       end
     end
   end
--- specs/constraints/distinct.rb
+++ specs/constraints/distinct.rb
@@ -7,7 +7,8 @@ class DistinctSampleProblem < Gecode::Model
   
   def initialize
     @vars = int_var_array(2, 1)
-    @sets = set_var_array(2, [], 0..1)
+    @sets = set_var_array(2, [], 0..2)
+    branch_on wrap_enum(@sets)
   end
 end
 
@@ -141,12 +142,67 @@ describe Gecode::Constraints::SetEnum::Distinct do
   
   it 'should not allow options other than :size' do
     lambda do
-      @sets.must_not_be.distinct(:size => @size, :foo => 17)
+      @sets.must_be.distinct(:size => @size, :foo => 17)
     end.should raise_error(ArgumentError)
   end
   
-  it 'should not calls that do not specify :size' do
+  it 'should raise error if :size is not specified' do
     lambda{ @sets.must_be.distinct }.should raise_error(ArgumentError)
+  end
+  
+  it_should_behave_like 'non-reifiable set constraint'
+end
+
+describe Gecode::Constraints::SetEnum::Distinct, ' (at most one)' do
+  before do
+    @model = DistinctSampleProblem.new
+    @sets = @model.sets
+    @size = 2
+    
+    @invoke_options = lambda do |hash| 
+      @sets.must.at_most_share_one_element hash.update(:size => @size)
+      @model.solve!
+    end
+    @expect_options = lambda do |strength, reif_var|
+      Gecode::Raw.should_receive(:atmostOne).once.with(@model.active_space, 
+        an_instance_of(Gecode::Raw::SetVarArray), @size)
+    end
+  end
+
+  it 'should translate into a atmostOne constraint' do
+    Gecode::Raw.should_receive(:atmostOne).once.with(@model.active_space, 
+      an_instance_of(Gecode::Raw::SetVarArray), @size)
+    @sets.must.at_most_share_one_element(:size => @size)
+    @model.solve!
+  end
+
+  it 'should constrain sets to have at most one element in common' do
+    @sets.must.at_most_share_one_element(:size => @size)
+    @sets[0].must_not_be.superset_of 0
+    solution = @model.solve!
+    solution.should_not be_nil
+    set1, set2 = solution.sets
+    set1.glb.size.should == @size
+    set2.glb.size.should == @size
+    (set1.glb & set2.glb).size.should <= 1
+  end
+
+  it 'should not allow negation' do
+    lambda do
+      @sets.must_not.at_most_share_one_element(:size => @size) 
+    end.should raise_error(Gecode::MissingConstraintError)
+  end
+  
+  it 'should not allow options other than :size' do
+    lambda do
+      @sets.must.at_most_share_one_element(:size => @size, :foo => 17) 
+    end.should raise_error(ArgumentError)
+  end
+  
+  it 'should raise error if :size is not specified' do
+    lambda do
+      @sets.must.at_most_share_one_element 
+    end.should raise_error(ArgumentError)
   end
   
   it_should_behave_like 'non-reifiable set constraint'
