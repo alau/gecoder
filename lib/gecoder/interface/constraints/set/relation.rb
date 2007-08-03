6176747a695dfdc8b70b9c119c103d853c389c2a
Andreas Launila <github@lokorin.org>
2007-08-03 09:47:18 +0000

    Expanded the RDoc documentation of the set relation constraints.

--- lib/gecoder/interface/constraints/set/relation.rb
+++ lib/gecoder/interface/constraints/set/relation.rb
@@ -49,8 +49,22 @@ module Gecode::Constraints::Set
   end
   
   # A module that gathers the classes and modules used in relation constraints.
-  module Relation
-    # Describes a relation constraint for equality.
+  module Relation #:nodoc:
+    # Describes a relation constraint which constrains a set variable to be 
+    # equal to another set variable. Equality may either be expressed as
+    # +==+, +equal+ or +equal_to+.
+    # 
+    # == Examples
+    # 
+    #   # +set_1+ must be equal to +set_2+
+    #   set_1.must == set_2
+    #   
+    #   # +set_1+ must not be equal to +set_2+
+    #   set_1.must_not == set_2
+    #   
+    #   # The same as above but reified with the boolean variable 
+    #   # +are_not_equal+.
+    #   set_1.must_not.equal(set_2, :reify => are_not_equal)
     class EqualityRelationConstraint < Gecode::Constraints::ReifiableConstraint
       def post
         var, rhs, reif_var, negate = @params.values_at(:lhs, :rhs, :reif, 
@@ -69,7 +83,29 @@ module Gecode::Constraints::Set
       end
     end
   
-    # Describes a relation constraint for the relations other than equality.
+    # Describes a relation constraint which constrains a set variable to have
+    # a specified relation to another set variable. The allowed relations and
+    # their aliases are
+    # 
+    # * subset, subset_of
+    # * superset, superset_of
+    # * disjoint, disjoint_with
+    # * complement, complement_of
+    # 
+    # == Examples
+    # 
+    #   # +set_1+ must be subset of +set_2+
+    #   set_1.must_be.subset_of set_2
+    #   
+    #   # +set_1+ must not be superset of +set_2+
+    #   set_1.must_not_be.superset_of set_2
+    #   
+    #   # +set_1+ must be disjoint with +set_2+
+    #   set_1.must_be.disjoint set_2
+    #   
+    #   # The same as above but reified with the boolean variable 
+    #   # +are_disjoint+.
+    #   set_1.must_be.disjoint(set_2, :reify => are_disjoint)
     class RelationConstraint < Gecode::Constraints::ReifiableConstraint
       def post
         var, rhs, reif_var, relation = @params.values_at(:lhs, :rhs, :reif, 
@@ -84,7 +120,24 @@ module Gecode::Constraints::Set
       negate_using_reification
     end
     
-    # Describes a relation constraint on the elements of a set.
+    # Describes an element relation constraint which constrains all elements in
+    # a set variable to satisfy an integer relation constraint. The relations
+    # supported are the same as in 
+    # <tt>Int::Linear::SimpleRelationConstraint</tt>.
+    # 
+    # Reification is not supported.
+    # 
+    # == Examples
+    # 
+    #   # All elements in +set+ must be larger than 5.
+    #   set.elements.must > 5
+    #   
+    #   # No element in +set+ may equal 0.
+    #   set.elements.must_not == 0
+    #   
+    #   # No element in +set+ may contain the value of the integer variable
+    #   # +forbidden_number+.
+    #   set.elements.must_not == forbidden_number
     class ElementRelationConstraint < Gecode::Constraints::Constraint
       def post
         var, rhs, relation = @params.values_at(:lhs, :rhs, :relation)
@@ -103,8 +156,8 @@ module Gecode::Constraints::Set
       end
     end
     
-    # Describes an expression which starts with set.element.must* .
-    class ElementExpression < Gecode::Constraints::Expression
+    # Describes an expression which starts with set.elements.must* .
+    class ElementExpression < Gecode::Constraints::Expression #:nodoc:
       Gecode::Constraints::Util::RELATION_TYPES.each_key do |name|
         module_eval <<-"end_code"
           # Creates an elements constraint using the specified expression, which
