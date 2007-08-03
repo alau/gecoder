49cd77fca1e42ea7744e9ffec2f4a2a89c229d7f
Andreas Launila <github@lokorin.org>
2007-08-03 10:33:05 +0000

    Expanded the RDoc documentation of the set selection constraints.

--- lib/gecoder/interface/constraints/set_enum/selection.rb
+++ lib/gecoder/interface/constraints/set_enum/selection.rb
@@ -2,7 +2,7 @@ module Gecode::SetEnumMethods
   # This adds the adder for the methods in the modules including it. The 
   # reason for doing it so indirect is that the first #[] won't be defined 
   # before the module that this is mixed into is mixed into an enum.
-  def self.included(mod)
+  def self.included(mod) #:nodoc:
     mod.module_eval do
       # Now we enter the module that the module possibly defining #[] 
       # is mixed into.
@@ -31,24 +31,10 @@ module Gecode::SetEnumMethods
 end
 
 # A module that gathers the classes and modules used by selection constraints.
-module Gecode::Constraints::SetEnum::Selection
-  # Describes an expression stub started with a set var enum following with an 
-  # array access using an integer variable.
-  class SelectExpressionStub < Gecode::Constraints::Set::CompositeStub
-    def constrain_equal(variable, params, constrain)
-      enum, index = @params.values_at(:lhs, :index)
-      if constrain
-        variable.must_be.subset_of enum.upper_bound_range
-      end
-
-      Gecode::Raw::selectSet(@model.active_space, enum.to_set_var_array, 
-        index.bind, variable.bind)
-    end
-  end
-  
+module Gecode::Constraints::SetEnum::Selection #:nodoc:
   # Describes an expression stub started with a set var enum followed with an
   # array access using a set variable.
-  class SetAccessStub < Gecode::Constraints::ExpressionStub
+  class SetAccessStub < Gecode::Constraints::ExpressionStub #:nodoc:
     include Gecode::Constraints::LeftHandSideMethods
     
     # Starts a union selection constraint on the selected sets.
@@ -85,8 +71,69 @@ module Gecode::Constraints::SetEnum::Selection
     end
   end
   
-  # Describes an expression stub started with a set var enum following with an 
-  # array access using a set variable followed by #union.
+  # Describes an expression that starts with an set variable enum followed with
+  # an array access using a set variable followed by some form of must.
+  class SetAccessExpression < Gecode::Constraints::Set::Expression #:nodoc:
+    # Constrains the selected sets to be disjoint.
+    def disjoint
+      if @params[:negate]
+        raise Gecode::MissingConstraintError, 'A negated set selection ' + 
+          'disjoint is not implemented.'
+      end
+      
+      @model.add_constraint DisjointConstraint.new(@model, @params)
+    end
+  end
+
+  # Describes a CompositeStub for the set select constraint, which constrains 
+  # the set in a position specified by an integer variable in an enumeration of 
+  # set variable.
+  # 
+  # == Examples
+  # 
+  #   # The set at the position described by the integer variable 
+  #   # +singleton_zero_position+ in the enumeration +sets+ of set variables 
+  #   # must equal [0].
+  #   sets[singleton_zero_position].must == 0
+  #   
+  #   # The set at the position described by the integer variable +position+ in 
+  #   # the enumeration +sets+ of set variables must be a subset of +set+.
+  #   sets[position].must_be.subset_of set
+  #   
+  #   # The same as above, but reified with the boolean variable +bool+.
+  #   sets[position].must_be.subset_of(set, :reify => bool)
+  class SelectExpressionStub < Gecode::Constraints::Set::CompositeStub
+    def constrain_equal(variable, params, constrain)
+      enum, index = @params.values_at(:lhs, :index)
+      if constrain
+        variable.must_be.subset_of enum.upper_bound_range
+      end
+
+      Gecode::Raw::selectSet(@model.active_space, enum.to_set_var_array, 
+        index.bind, variable.bind)
+    end
+  end
+  
+  # Describes a CompositeStub for the set union selection constraint, 
+  # which constrains the union of sets located at the positions 
+  # specified by a set variable in an enumeration of set variables.
+  # 
+  # == Examples
+  # 
+  #   # The sets in the enumeration set variable +sets+ located at the positions
+  #   # described by the set variable +selected_sets+ must have a union that's
+  #   # a superset of [0,4,17]. 
+  #   sets[selected_sets].union.must_be.superset_of [0,4,17]
+  #   
+  #   # The sets in the enumeration set variable +sets+ located at the positions
+  #   # described by the set variable +selected_sets+ must have a union that's
+  #   # disjoint with the set variable +set+.
+  #   sets[selected_sets].union.must_be.disjoint_with set
+  #   
+  #   # The same as above but reified with the boolean variable 
+  #   # +union_is_disjoint+.
+  #   sets[selected_sets].union.must_be.disjoint_with(set, 
+  #     :reify => union_is_disjoin)
   class UnionExpressionStub < Gecode::Constraints::Set::CompositeStub
     def constrain_equal(variable, params, constrain)
       enum, indices = @params.values_at(:lhs, :indices)
@@ -99,8 +146,40 @@ module Gecode::Constraints::SetEnum::Selection
     end
   end
   
-  # Describes an expression stub started with a set var enum following with an 
-  # array access using a set variable followed by #intersection.
+  # Describes a CompositeStub for the set intersection selection constraint, 
+  # which constrains the intersection of sets located at the positions 
+  # specified by a set variable in an enumeration of set variables.
+  # 
+  # Optionally a universe may also be specified.
+  # 
+  # == Examples
+  # 
+  #   # The sets in the enumeration set variable +sets+ located at the positions
+  #   # described by the set variable +selected_sets+ must have an intersection 
+  #   # that's a superset of [0,4,17]. 
+  #   sets[selected_sets].intersection.must_be.superset_of [0,4,17]
+  #   
+  #   # The sets in the enumeration set variable +sets+ located at the positions
+  #   # described by the set variable +selected_sets+ must have an intersection
+  #   # that's disjoint with the set variable +set+.
+  #   sets[selected_sets].intersection.must_be.disjoint_with set
+  #   
+  #   # The sets in the enumeration set variable +sets+ located at the positions
+  #   # described by the set variable +selected_sets+ must have an intersection
+  #   # that's disjoint with the set variable +set+ inside the universe 0..17.
+  #   sets[selected_sets].intersection(:with => 0..17).must_be.disjoint_with set
+  #   
+  #   # The sets in the enumeration set variable +sets+ located at the positions
+  #   # described by the set variable +selected_sets+ must have an intersection
+  #   # that's disjoint with the set variable +set+ inside the universe 
+  #   # described by the set variable +universe+.
+  #   sets[selected_sets].intersection(:with => universe).must_be.disjoint_with set
+  #   
+  #   
+  #   # The same as above but reified with the boolean variable 
+  #   # +intersection_is_disjoint+.
+  #   sets[selected_sets].intersection(:with => universe).must_be.disjoint_with(
+  #     set, :reifty => intersection_is_disjoin)
   class IntersectionExpressionStub < Gecode::Constraints::Set::CompositeStub
     def constrain_equal(variable, params, constrain)
       enum, indices, universe = @params.values_at(:lhs, :indices, :universe)
@@ -118,21 +197,16 @@ module Gecode::Constraints::SetEnum::Selection
     end
   end
   
-  # Describes an expression that starts with an set variable enum followed with
-  # an array access using a set variable followed by some form of must.
-  class SetAccessExpression < Gecode::Constraints::Set::Expression
-    # Constrains the selected sets to be disjoint.
-    def disjoint
-      if @params[:negate]
-        raise Gecode::MissingConstraintError, 'A negated set selection ' + 
-          'disjoint is not implemented.'
-      end
-      
-      @model.add_constraint DisjointConstraint.new(@model, @params)
-    end
-  end
-  
-  # Describes a disjoint constraint produced by sets[set].must_be.disjoint .
+  # Describes a disjoint constraint, which constrains all set variable is an
+  # enumeration, at the position specified by a set variable, to be disjoint.
+  # 
+  # Does not support negation nor reification.
+  # 
+  # == Examples
+  # 
+  #   # The set variable located in the enumeration +sets+ at positions 
+  #   # described by +disjoint_set_positions+ must be disjoint.
+  #   sets[disjoint_set_positions].must_be.disjoint 
   class DisjointConstraint < Gecode::Constraints::Constraint
     def post
       enum, indices = @params.values_at(:lhs, :indices)
