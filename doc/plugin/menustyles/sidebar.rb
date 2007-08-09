baa2a8cff09d67840344c746b414c659bd2380f1
Andreas Launila <github@lokorin.org>
2007-08-09 13:53:58 +0000

    The sidebar is now sorted in order of orderinfo.

--- doc/plugin/menustyles/sidebar.rb
+++ doc/plugin/menustyles/sidebar.rb
@@ -8,14 +8,31 @@ class SidbarMenuStyle < MenuStyles::DefaultMenuStyle
 
   def internal_build_menu(src_node, menu_tree)
     if src_node.to_url.to_s =~ /index\.html/
-      menu = src_node.parent.inject('<ul id="secondNav">') do |out, child|
-        next out if child == src_node or 
-          child.to_url.to_s =~ /\.template$|\.page$|\.xml$|\.css$|images/
-        out << "<li>#{child.link_from(src_node)}</li>"
-      end << '</ul>'
-      "<h3>#{src_node.parent['title']}</h3>#{menu}"
+      node = find_menu_node(menu_tree, src_node)
+      return '' if node.parent.nil?
+
+      menu = node.parent.inject('') do |out, child|
+        next out if child.node_info[:node] == src_node
+        _, link = menu_item_details(src_node, child.node_info[:node])
+        out << "<li>#{link}</li>" 
+      end
+      "<h3>#{src_node.parent['title']}</h3><ul id=\"secondNav\">#{menu}</ul>"
     else
       ''
     end
+  end
+  
+  private
+  
+  # Finds the node in the menu tree that corresponds to the specified node.
+  def find_menu_node(menu_tree, node_to_find)
+    match = node = menu_tree
+    until match.nil?
+      node = match
+      match = node.find do |child| 
+        node_to_find.in_subtree_of? child.node_info[:node]
+      end
+    end
+    node
   end
 end
