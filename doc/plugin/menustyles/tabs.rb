276426cc2fae8c85aa0c3b941ccaaff161ff2d2b
Andreas Launila <github@lokorin.org>
2007-08-09 13:54:41 +0000

    Named the dummy variable "_" to signify that it's a dummy variable.

--- doc/plugin/menustyles/tabs.rb
+++ doc/plugin/menustyles/tabs.rb
@@ -9,7 +9,7 @@ class TabMenuStyle < MenuStyles::DefaultMenuStyle
   def internal_build_menu(src_node, menu_tree)
     menu_tree.inject('<div id="tabs"><ul>') do |out, child|
       node = child.node_info[:node]
-      style, link = menu_item_details(src_node, node)
+      _, link = menu_item_details(src_node, node)
       if node == src_node || (node.is_directory? && src_node.in_subtree_of?(node))
         out << "<li class=\"selected\">#{link}</li>"
       else
