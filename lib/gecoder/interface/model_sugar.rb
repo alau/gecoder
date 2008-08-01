6bf80800eb97f83cb8d9c62f1dc57599e7e0edf9
Andreas Launila <github@lokorin.org>
2008-08-01 18:09:23 +0000

    Added Gecode.minimize and Gecode.maximize .

--- lib/gecoder/interface/model_sugar.rb
+++ lib/gecoder/interface/model_sugar.rb
@@ -10,7 +10,7 @@ module Gecode
   #     # Do something
   #   end
   # 
-  # corresponds to
+  # is equivalent to
   # 
   #   class Foo < Gecode::Model
   #     def initialize
@@ -19,12 +19,66 @@ module Gecode
   #   end
   #   solution = Foo.new.solve!
   def self.solve(&block)
+    create_model(&block).solve!
+  end
+
+  # Provides a convenient way to construct a model and then find the
+  # solution that maximizes a given variable. The model constructed 
+  # uses the specified block as initialization method. The solution
+  # that maximizes the specified variable is then returned.
+  #
+  # For instance
+  #
+  #   solution = Gecode.maximize :variable_bar do
+  #     # Do something
+  #   end
+  # 
+  # is equivalent to
+  # 
+  #   class Foo < Gecode::Model
+  #     def initialize
+  #       # Do something
+  #     end
+  #   end
+  #   solution = Foo.new.maximize :variable_bar
+  def self.maximize(variable_to_maximize, &block)
+    create_model(&block).maximize! variable_to_maximize
+  end
+
+  # Provides a convenient way to construct a model and then find the
+  # solution that minimizes a given variable. The model constructed 
+  # uses the specified block as initialization method. The solution
+  # that minimizes the specified variable is then returned.
+  #
+  # For instance
+  #
+  #   solution = Gecode.minimize :variable_bar do
+  #     # Do something
+  #   end
+  # 
+  # is equivalent to
+  # 
+  #   class Foo < Gecode::Model
+  #     def initialize
+  #       # Do something
+  #     end
+  #   end
+  #   solution = Foo.new.minimize :variable_bar
+  def self.minimize(variable_to_minimize, &block)
+    create_model(&block).minimize! variable_to_minimize
+  end
+
+  private
+
+  # Creates an instance of a class that subclasses Model and uses the 
+  # specified block as initialization method.
+  def self.create_model(&block)
     model = Class.new(Gecode::Model)
     model.class_eval do
       def initialize(&init_block)
         instance_eval &init_block
       end
     end
-    model.new(&block).solve!
+    model.new(&block)
   end
 end
--- specs/model_sugar.rb
+++ specs/model_sugar.rb
@@ -9,4 +9,22 @@ describe Gecode, ' (Model sugar)' do
       branch_on numbers
     end.numbers.values.should == [5,5]
   end
+
+  it 'should provide #maximize as sugar for constructing a model and running maximize!' do
+    Gecode.maximize :z do
+      z_is_an int_var
+      x, y = vars = int_var_array(2, 0..5)
+      (x*2 - y).must == z
+      branch_on vars
+    end.z.value.should equal(10)
+  end
+
+  it 'should provide #minimize as sugar for constructing a model and running minimize!' do
+    Gecode.minimize :z do
+      z_is_an int_var
+      x, y = vars = int_var_array(2, 0..5)
+      (x*2 - y).must == z
+      branch_on vars
+    end.z.value.should equal(-5)
+  end
 end
